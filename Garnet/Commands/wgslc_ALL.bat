8facdcdbc77089c9b83b9bc1191aaa0dc59a0804
HaruoOgawa <haru.spring.yodai@gmail.com>
2023-05-03 16:32:50 +0900

    shader構成の整理

--- Garnet/Commands/wgslc_ALL.bat
+++ Garnet/Commands/wgslc_ALL.bat
@@ -2,8 +2,6 @@ set CurrentDir=%~dp0
 set ShaderDir=..\Resources\Shaders
 set EXEDir=..\src\Library\Vulkan\bin\
 
-for /R %ShaderDir% %%i in (*vert.spv) do naga %%~i %%~pi%%~ni.wgsl 
-
-for /R %ShaderDir% %%i in (*frag.spv) do naga %%~i %%~pi%%~ni.wgsl 
+for /R %ShaderDir% %%i in (*.spv) do naga %%~i %%~pi%%~ni.wgsl 
 
 pause
