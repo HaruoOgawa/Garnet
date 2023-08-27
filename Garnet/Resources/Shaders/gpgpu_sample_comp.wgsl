bba22746881db220ba6e8e22561fa4de6f62f87b
HaruoOgawa <haru.spring.yodai@gmail.com>
2023-08-28 08:57:37 +0900

    WebGPUだけスピードが異なる問題を修正

--- Garnet/Resources/Shaders/gpgpu_sample_comp.wgsl
+++ Garnet/Resources/Shaders/gpgpu_sample_comp.wgsl
@@ -14,6 +14,8 @@ struct WriteOnlyTestBufferObject {
 struct ParamUBO {
     time: f32,
     deltaTime: f32,
+    pad0_: f32,
+    pad1_: f32,
 }
 
 struct ReadOnlyTestBufferObject {
@@ -29,8 +31,8 @@ var<uniform> ubo: ParamUBO;
 var<storage> r_TBO: ReadOnlyTestBufferObject;
 
 fn randvf2_(st: ptr<function, vec2<f32>>) -> f32 {
-    let _e20 = (*st);
-    return fract((sin(dot(_e20, vec2<f32>(12.989800453186035, 78.23300170898438))) * 43758.546875));
+    let _e21 = (*st);
+    return fract((sin(dot(_e21, vec2<f32>(12.989800453186035, 78.23300170898438))) * 43758.546875));
 }
 
 fn main_1() {
@@ -38,18 +40,18 @@ fn main_1() {
     var id_f: f32;
     var param: vec2<f32>;
 
-    let _e23 = gl_GlobalInvocationID_1[0u];
-    id = _e23;
-    let _e24 = id;
-    id_f = f32(_e24);
-    let _e26 = id;
-    let _e28 = ubo.time;
-    let _e29 = id_f;
+    let _e24 = gl_GlobalInvocationID_1[0u];
+    id = _e24;
+    let _e25 = id;
+    id_f = f32(_e25);
+    let _e27 = id;
+    let _e29 = ubo.time;
     let _e30 = id_f;
-    param = vec2<f32>(_e29, (_e30 + 12.394000053405762));
-    let _e33 = randvf2_((&param));
-    let _e41 = w_TBO.data[_e26].offset[1u];
-    w_TBO.data[_e26].offset[1u] = (_e41 + sin((_e28 + (_e33 * 10.0))));
+    let _e31 = id_f;
+    param = vec2<f32>(_e30, (_e31 + 12.394000053405762));
+    let _e34 = randvf2_((&param));
+    let _e43 = w_TBO.data[_e27].offset[1u];
+    w_TBO.data[_e27].offset[1u] = (_e43 + (sin((_e29 + (_e34 * 10.0))) * 0.10000000149011612));
     return;
 }
 
--- Garnet/Src/GraphicsAPI/Vulkan/CVulkanMaterial.cpp
+++ Garnet/Src/GraphicsAPI/Vulkan/CVulkanMaterial.cpp
@@ -76,6 +76,8 @@ namespace api
 			// SharedBufferは処理しない
 			if (m_ShaderBufferList[i]->GetSharedBufferParam().IsShared) continue;
 
+			if (m_ShaderBufferList[i]->GetBufferType() == graphics::EBufferType::SHADERSTORAGE) continue;
+
 			auto ByteSize = m_VKUniformBufferSizeList[m_pGraphicsAPI->GetCurrentFrame()][i];
 			auto ByteOffset = ((m_UseDynamicBufferOffset)? (DynamicOffsetNum - 1) * ByteSize : 0);
 
@@ -317,8 +319,8 @@ namespace api
 				const auto& Data = Buffer->GetData();
 				const uint32_t ByteSize = static_cast<uint32_t>(math::GetNextPowerOfTwo(static_cast<unsigned int>(Data.size()))); // 2のn乗にする
 
-				VkBuffer UniformBuffer;
-				VkDeviceMemory BufferMemory;
+				VkBuffer UniformBuffer = nullptr;
+				VkDeviceMemory BufferMemory = nullptr;
 
 				// バッファの作成
 				if (Buffer->GetBufferType() == graphics::EBufferType::UNIFORM)
@@ -344,7 +346,18 @@ namespace api
 					}
 				}
 
-				//
+				// バッファの初期化
+				if (Buffer->GetBufferType() == graphics::EBufferType::SHADERSTORAGE)
+				{
+					// ひとまずDynamicOffset周りでいろいろ問題が起こりそうなのでSSBOの時だけ初期化する
+					// (様子をみて変えるかも)
+					void* BuffersMappedList;
+					vkMapMemory(m_pGraphicsAPI->GetLogicalDevice(), BufferMemory, 0, ByteSize, 0, &BuffersMappedList);
+					std::memcpy(BuffersMappedList, &Data[0], ByteSize);
+					vkUnmapMemory(m_pGraphicsAPI->GetLogicalDevice(), BufferMemory);
+				}
+
+				// バッファをリストに追加
 				m_VKUniformBufferList[i].push_back(UniformBuffer);
 				m_VKUniformBufferMemoryList[i].push_back(BufferMemory);
 				m_VKUniformBufferSizeList[i].push_back(ByteSize);
