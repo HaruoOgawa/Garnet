925b80651b984d3cfa198ab958bb0175c0c876fd
HaruoOgawa <haru.spring.yodai@gmail.com>
2023-12-03 11:54:19 +0900

    ReplaceDataのリファクタリング

--- Garnet/Src/Graphics/CShaderBuffer.cpp
+++ Garnet/Src/Graphics/CShaderBuffer.cpp
@@ -54,17 +54,10 @@ namespace graphics
 
 	void CShaderBuffer::ReplaceData(const std::string& SrcName, const void* SrcData, int SrcByteSize, int BindingIndex)
 	{
-		const auto& Layout = m_BindingLayoutList.find(BindingIndex);
-
-		if (BindingIndex < 0 || Layout == m_BindingLayoutList.end())
+		// BindingIndexが登録されていないものなら処理しない
+		if (BindingIndex < 0 || m_BindingLayoutList.find(BindingIndex) == m_BindingLayoutList.end())
 		{
-			Console::Log("[ERROR] BindingIndex is out of range. %d\n", BindingIndex);
 			return;
-		}
-		else if (BindingIndex >= 1 && m_BindingLayoutList.size() >= 2 && m_BindingLayoutList[BindingIndex - 1].ByteSize < 256)
-		{
-			Console::Log("[WARNING] UBO(%d) is smaller than 256 byte.(UBO ByteSize: %d) But you are going to use the following buffer.\n",
-				BindingIndex - 1, m_BindingLayoutList[BindingIndex - 1].ByteSize);
 		}
 
 		// 既にBufferに追加済みのデータを更新する
