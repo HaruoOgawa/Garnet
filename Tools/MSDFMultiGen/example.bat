6239370af36e6456421d97c4e1623724294d822e
HaruoOgawa <haru.spring.yodai@gmail.com>
2024-07-27 17:00:10 +0900

    Win実行ファイル版を作成

--- Tools/MSDFMultiGen/example.bat
+++ Tools/MSDFMultiGen/example.bat
@@ -1,3 +1,5 @@
-python MSDFMultiGen.py -c "msdfgen.exe" -i "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" -o "./dist/msdf.png" -f "C:\Windows\Fonts\arialbd.ttf" -dw 32 -dh 32 -m sdf
+@rem python MSDFMultiGen.py -c "msdfgen.exe" -i "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" -o "./dist/msdf.png" -f "C:\Windows\Fonts\arialbd.ttf" -dw 32 -dh 32 -m sdf
+
+MSDFMultiGen.exe -c "msdfgen.exe" -i "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" -o "./dist/msdf.png" -f "C:\Windows\Fonts\arialbd.ttf" -dw 32 -dh 32 -m sdf
 
 pause
