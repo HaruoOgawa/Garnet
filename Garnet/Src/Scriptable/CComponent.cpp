21fa337c3b544354960ff83001a23845445e37a2
HaruoOgawa <haru.spring.yodai@gmail.com>
2024-09-16 16:04:42 +0900

    Componentの基礎実装

--- Garnet/Src/Scriptable/CComponent.cpp
+++ Garnet/Src/Scriptable/CComponent.cpp
@@ -2,5 +2,38 @@
 
 namespace scriptable
 {
+	CComponent::CComponent(const std::string& Name):
+		CValueRegistry(Name),
+		m_Name(Name)
+	{
+	}
 
+	CComponent::~CComponent()
+	{
+	}
+
+	const std::string CComponent::GetName() const
+	{
+		return m_Name;
+	}
+
+	void CComponent::OnLoaded(const std::shared_ptr<scene::CSceneController>& SceneController)
+	{
+	}
+
+	bool CComponent::Initialize(api::IGraphicsAPI* pGraphicsAPI, resource::CLoadWorker* pLoadWorker)
+	{
+		return true;
+	}
+
+	bool CComponent::Update(api::IGraphicsAPI* pGraphicsAPI, physics::IPhysicsEngine* pPhysicsEngine, resource::CLoadWorker* pLoadWorker, const std::shared_ptr<camera::CCamera>& Camera, const std::shared_ptr<projection::CProjection>& Projection,
+		const std::shared_ptr<graphics::CDrawInfo>& DrawInfo, const std::shared_ptr<input::CInputState>& InputState)
+	{
+		return true;
+	}
+
+	bool CComponent::Draw(api::IGraphicsAPI* pGraphicsAPI, const std::shared_ptr<camera::CCamera>& Camera, const std::shared_ptr<projection::CProjection>& Projection, const std::shared_ptr<graphics::CDrawInfo>& DrawInfo)
+	{
+		return true;
+	}
 }
--- Garnet/Src/Scriptable/CComponent.h
+++ Garnet/Src/Scriptable/CComponent.h
@@ -1,9 +1,38 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+#include <tuple>
+#include <Interface/IGraphicsAPI.h>
+#include <Scriptable/CValueRegistry.h>
+
+namespace resource { class CLoadWorker; }
+namespace camera { class CCamera; }
+namespace projection { class CProjection; }
+namespace input { class CInputState; }
+namespace physics { class IPhysicsEngine; }
+namespace scene { class CSceneController; }
+namespace graphics { class CDrawInfo; }
+
 namespace scriptable
 {
-	class CComponent
+	class CComponent : public CValueRegistry
 	{
-	};
+		const std::string m_Name;
+	public:
+		CComponent(const std::string& Name);
+		virtual ~CComponent();
 
+		const std::string GetName() const;
+
+		virtual void OnLoaded(const std::shared_ptr<scene::CSceneController>& SceneController);
+
+		virtual bool Initialize(api::IGraphicsAPI* pGraphicsAPI, resource::CLoadWorker* pLoadWorker);
+
+		virtual bool Update(api::IGraphicsAPI* pGraphicsAPI, physics::IPhysicsEngine* pPhysicsEngine, resource::CLoadWorker* pLoadWorker, const std::shared_ptr<camera::CCamera>& Camera, const std::shared_ptr<projection::CProjection>& Projection,
+			const std::shared_ptr<graphics::CDrawInfo>& DrawInfo, const std::shared_ptr<input::CInputState>& InputState);
+
+		virtual bool Draw(api::IGraphicsAPI* pGraphicsAPI, const std::shared_ptr<camera::CCamera>& Camera, const std::shared_ptr<projection::CProjection>& Projection,
+			const std::shared_ptr<graphics::CDrawInfo>& DrawInfo);
+	};
 }
