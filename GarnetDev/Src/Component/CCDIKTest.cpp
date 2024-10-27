744e25143602f57a5b5bc448f353afdee44e95ad
HaruoOgawa <haru.spring.yodai@gmail.com>
2024-10-27 18:52:38 +0900

    せん断を解消(モデル側の問題でスケールを全て１にした)

--- GarnetDev/Src/Component/CCDIKTest.cpp
+++ GarnetDev/Src/Component/CCDIKTest.cpp
@@ -39,12 +39,6 @@ namespace component
 			m_LinkList.push_back(LinkNode);
 		}
 
-		// あれ、直で回転入れても何も反応しない
-		glm::quat rot = glm::angleAxis(3.1415f / 2.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-		//m_LinkList[m_NumOfLink - 1]->SetRot(rot);
-		// 2個前にすると反応。でも45度とかにするとせん断が発生する
-		//m_LinkList[m_NumOfLink - 2]->SetRot(rot);
-
 		return true;
 	}
 
@@ -52,50 +46,21 @@ namespace component
 		const std::shared_ptr<graphics::CDrawInfo>& DrawInfo, const std::shared_ptr<input::CInputState>& InputState)
 	{
 		if (!m_TargetNode || m_LinkList.empty()) return true;
-
-		// マウス位置にターゲットを追従させる
-		/*if (InputState->IsKeyDown(input::EKeyType::KEY_TYPE_1))
-		{
-			glm::vec2 Res = Projection->GetScreenResolution();
-
-			// マウスのスクリーン座標を計算
-			glm::vec2 MousePos = InputState->GetMousePos();
-			//MousePos.x *= Res.y / Res.x;
-			glm::vec4 MouseScreenPos = glm::vec4(MousePos.x, -MousePos.y, 0.0f, 1.0f); // カメラにぴったりくっついているものとして考えるのでZを0.0fとしている
-			
-			glm::vec4 MouseViewPos = glm::inverse(Projection->GetPrejectionMatrix()) * MouseScreenPos;
-			
-			// 射影除算を行う
-			Console::Log("[Prev] MouseViewPos => x: %f, y: %f, z: %f, w: %f\n", MouseViewPos.x, MouseViewPos.y, MouseViewPos.z, MouseViewPos.w);
-
-			MouseViewPos /= MouseViewPos.w;
-
-			Console::Log("[After] MouseViewPos => x: %f, y: %f, z: %f, w: %f\n", MouseViewPos.x, MouseViewPos.y, MouseViewPos.z, MouseViewPos.w);
-
-			glm::vec4 MouseWorldPos = glm::inverse(Camera->GetViewMatrix()) * MouseViewPos;
-
-			Console::Log("[After] MouseWorldPos => x: %f, y: %f, z: %f, w: %f\n", MouseWorldPos.x, MouseWorldPos.y, MouseWorldPos.z, MouseWorldPos.w);
-
-			glm::vec3 Pos = glm::vec3(MouseWorldPos.x, MouseWorldPos.y, MouseWorldPos.z);
-
-			// カメラとターゲットの初期値の
-			Pos += (m_InitTargetPos - Camera->GetPos());
-
-			Console::Log("[After] Pos => x: %f, y: %f, z: %f\n", Pos.x, Pos.y, Pos.z);
-
-			m_TargetNode->SetPos(Pos);
-		}*/
-
+		
+		// ターゲットのアニメーション
 		{
 			float r = 2.0f;
 
 			glm::vec3 Pos = m_TargetNode->GetPos();
 			Pos.x = r * glm::cos(DrawInfo->GetSecondsTime());
 			Pos.y = r * glm::abs(glm::sin(DrawInfo->GetSecondsTime()));
+			Pos.z = r * glm::sin(DrawInfo->GetSecondsTime());
 
 			m_TargetNode->SetPos(Pos);
 		}
 
+		// 注意点
+		// * せん断が発生してしまうのでCCDIKのリンクのスケールは必ず(1, 1, 1)になるようにする
 		const glm::vec3 TargetPos = m_TargetNode->GetWorldPos();
 
 		int NumOfCicle = 64;
@@ -143,34 +108,21 @@ namespace component
 							SubAxis = glm::cross(YAxis, e_i);
 						}
 
-						rot = glm::angleAxis(3.1415f, glm::normalize(SubAxis)); // 回転取得前にちゃんと軸を正規化しておく
-
-						if (std::isnan(rot.x) || std::isnan(rot.y) || std::isnan(rot.z) || std::isnan(rot.w))
-						{
-							float x = 0.0f;
-						}
+						rot = glm::angleAxis(3.1415f, glm::normalize(SubAxis)); // 回転角度がおかしくなってしまうので回転取得前にちゃんと軸を正規化しておく
 					}
 				}
 				else
 				{
 					// 通常通り内積結果から回転
 					float angle = glm::acos(dot);
-					rot = glm::angleAxis(angle, glm::normalize(axis)); // 回転取得前にちゃんと軸を正規化しておく
-
-					if (std::isnan(rot.x) || std::isnan(rot.y) || std::isnan(rot.z) || std::isnan(rot.w))
-					{
-						float x = 0.0f;
-					}
+					rot = glm::angleAxis(angle, glm::normalize(axis)); // 回転角度がおかしくなってしまうので回転取得前にちゃんと軸を正規化しておく
 				}
 
 				if (std::isnan(rot.x) || std::isnan(rot.y) || std::isnan(rot.z) || std::isnan(rot.w))
 				{
-					float x = 0.0f;
+					Console::Log("[Error] CCDIK - found NaN value in ik rot.\n");
+					return false;
 				}
-
-				// 現在のLinkよりも１つ先端に近い方に回転を反映する
-				//std::shared_ptr<object::CNode> PrevLinkNode = m_LinkList[i + 1];
-				//PrevLinkNode->SetRot(rot * PrevLinkNode->GetRot());
 
 				LinkNode->SetRot(rot * LinkNode->GetRot());
 
