2873e183db263acbecbd7ecdde5ce7101b4667f5
haruoogawa <haru.spring.yodai@gmail.com>
2024-11-02 14:55:19 +0900

    CCDIKのテスト実装

--- Garnet/Src/Animation/CIKSolver.cpp
+++ Garnet/Src/Animation/CIKSolver.cpp
@@ -36,6 +36,7 @@ namespace animation
 		int EndEffectorIndex = m_IKParam->IKTargetBoneIndex;
 		if (EndEffectorIndex < 0 || EndEffectorIndex >= BoneList.size()) return false;
 
+		// 根本から先端の方向でLinkNodeが入っている
 		m_IKChainList.push_back(BoneList[EndEffectorIndex]->GetBoneNode());
 
 		return true;
@@ -44,7 +45,9 @@ namespace animation
 	bool CIKSolver::Solve()
 	{
 		// CCD-IKを採用
-		int NumOfLink = static_cast<int>(m_IKChainList.size());
+		const int NumOfLink = static_cast<int>(m_IKChainList.size());
+
+		const int EndIndex = NumOfLink - 1;
 
 		float Threshold = 0.01f;
 
@@ -56,7 +59,7 @@ namespace animation
 
 		const int MaxLoopNum = m_IKParam->IKLoopCount;
 
-		std::shared_ptr<object::CNode> EndNode = m_IKChainList[NumOfLink - 1];
+		std::shared_ptr<object::CNode> EndNode = m_IKChainList[EndIndex];
 
 		while (DoLoop && CurrentLoopNum < MaxLoopNum)
 		{
@@ -87,6 +90,8 @@ namespace animation
 
 				glm::quat rot;
 
+				// EffectVecとTargetVecがほぼ平行なので回転軸が存在しない(ほぼ0)になっていることがあるのでそれを考慮する
+				// ほぼ平行の時は任意な垂直時軸に対して0度か180度回転させる
 				if (glm::length(axis) < 1e-6f)
 				{
 					if (glm::sign(dot) == 1.0f)
@@ -99,13 +104,20 @@ namespace animation
 						// 反対方向に平行なので任意の垂直軸で180度回転する
 						glm::vec3 XAxis = glm::vec3(1.0f, 0.0f, 0.0f);
 						glm::vec3 YAxis = glm::vec3(0.0f, 1.0f, 0.0f);
+						glm::vec3 ZAxis = glm::vec3(0.0f, 0.0f, 1.0f);
 
 						glm::vec3 SubAxis = glm::cross(XAxis, e_i);
 
 						if (glm::length(SubAxis) < 1e-6f)
 						{
-							// X軸とも平行なのでY軸の方を使う(さすがにXとYを見れば大丈夫なはず？)
+							// X軸とも平行なのでY軸の方を使う
 							SubAxis = glm::cross(YAxis, e_i);
+
+							if (glm::length(SubAxis) < 1e-6f)
+							{
+								// Y軸とも平行なのでZ軸の方を使う
+								SubAxis = glm::cross(ZAxis, e_i);
+							}
 						}
 
 						rot = glm::angleAxis(3.1415f, glm::normalize(SubAxis)); // 回転角度がおかしくなってしまうので回転取得前にちゃんと軸を正規化しておく
@@ -125,10 +137,12 @@ namespace animation
 				// 回転角度制限
 				// 制限を行うことで例えば膝が変な方向に曲がらないようにする
 				int LinkIndex = static_cast<int>(m_IKParam->IKLinkList.size()) - 1 - i;
-				if (m_IKParam->IKLinkList[LinkIndex].IsLimitAngle)
+				const auto& IKLink = m_IKParam->IKLinkList[LinkIndex];
+
+				if (IKLink.IsLimitAngle)
 				{
-					const auto& LowerAngle = m_IKParam->IKLinkList[LinkIndex].LowerAngle;
-					const auto& UpperAngle = m_IKParam->IKLinkList[LinkIndex].UpperAngle;
+					const auto& LowerAngle = IKLink.LowerAngle;
+					const auto& UpperAngle = IKLink.UpperAngle;
 
 					glm::vec3 euler = glm::eulerAngles(rot);
 
@@ -182,6 +196,50 @@ namespace animation
 			// ループ回数を更新
 			CurrentLoopNum++;
 		}
+
+		/*// 回転角度制限
+		// 制限を行うことで例えば膝が変な方向に曲がらないようにする
+		const int NumOfIKLink = static_cast<int>(m_IKParam->IKLinkList.size());
+
+		for (int LinkIndex = NumOfIKLink - 1; LinkIndex >= 0; LinkIndex--)
+		{
+			const auto& IKLink = m_IKParam->IKLinkList[LinkIndex];
+
+			// m_IKChainListにはIKLinkListの先頭から順番に入れている
+			std::shared_ptr<object::CNode> ReCalcNode = m_IKChainList[NumOfIKLink - LinkIndex - 1];
+
+			glm::quat rot = ReCalcNode->GetRot();
+			
+			if (IKLink.IsLimitAngle)
+			{
+				const auto& LowerAngle = IKLink.LowerAngle;
+				const auto& UpperAngle = IKLink.UpperAngle;
+
+				glm::vec3 euler = glm::eulerAngles(rot);
+
+				// オイラー角に対して角度制限を行う
+				// LowerAngleとUpperAngleはラジアン
+				euler.x = glm::clamp(euler.x, LowerAngle.x, UpperAngle.x);
+				euler.y = glm::clamp(euler.y, LowerAngle.y, UpperAngle.y);
+				euler.z = glm::clamp(euler.z, LowerAngle.z, UpperAngle.z);
+
+				rot = glm::quat(euler);
+			}
+
+			ReCalcNode->SetRot(rot);
+
+			const auto& ParentNode = ReCalcNode->GetParentNode();
+			if (!ParentNode)
+			{
+				// 親ノードがない時はローカル行列をワールド行列として渡す
+				ReCalcNode->SetWorldMatrix(ReCalcNode->GetLocalMatrix());
+
+				continue;
+			}
+
+			glm::mat4 NewWorldMatrix = ParentNode->GetWorldMatrix() * ReCalcNode->GetLocalMatrix();
+			ReCalcNode->SetWorldMatrix(NewWorldMatrix);
+		}*/
 
 		return true;
 	}
