9a08358075d4ee4693cadf47a162449c42b0b34b
haruoogawa <haru.spring.yodai@gmail.com>
2025-01-31 08:21:30 +0900

    アニメーションのIsEndが正常動作していなかったので修正

--- Garnet/Src/Animation/CAnimationSampler.cpp
+++ Garnet/Src/Animation/CAnimationSampler.cpp
@@ -149,7 +149,7 @@ namespace animation
 
 	bool CAnimationSampler::IsEnd(float CurrentTime)
 	{
-		return (CurrentTime > m_EndTime);
+		return (CurrentTime >= m_EndTime);
 	}
 
 	std::vector<float> CAnimationSampler::CopyFromNumComponent(int NumComponent, const std::vector<float>& Src, int Offset)
