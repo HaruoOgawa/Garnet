19855c5c4b2abc18111d5fe99d37162706198d9d
haruoogawa <haru.spring.yodai@gmail.com>
2025-02-11 13:59:35 +0900

    テクスチャの範囲外チェックがうまくいっておらず、不本意でメッシュが透明になっていたものがあったので修正

--- Garnet/Src/MMD/PMX/CPmxImporter.cpp
+++ Garnet/Src/MMD/PMX/CPmxImporter.cpp
@@ -280,7 +280,7 @@ namespace mmd
 				{
 					int TextureIndex = PmxMaterial->GetMainTexIndex();
 
-					if (TextureIndex != -1)
+					if (TextureIndex >= 0 && TextureIndex < model.GetPmxTextureList().size())
 					{
 						material->ReplacePreloadUniformValue("UseMainTexture", &glm::ivec1(1)[0], sizeof(int), 2);
 						material->ReplaceTextureIndex("MainTexture", TextureIndex);
